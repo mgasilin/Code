a5e66ae6fb36373521b148acec4a77cc34792e10
mgasilin <95385096+mgasilin@users.noreply.github.com>
2025-11-04 02:08:14 +0300

    Delete dist directory

--- dist/common/base/controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-import { ApiResponse } from '../types';
-export declare class BaseController {
-    protected getUser(req: Request): any;
-    protected successResponse(data?: any, message?: string): ApiResponse;
-    protected errorResponse(error: string): ApiResponse;
-    protected paginatedResponse(data: any[], pagination: any): ApiResponse;
-}
-//# sourceMappingURL=controller.d.ts.map
--- dist/common/middleware/roles.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const roleMiddleware: (...allowedRoles: string[]) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=roles.d.ts.map
